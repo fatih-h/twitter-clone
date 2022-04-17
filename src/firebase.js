61c143c0ef968cc8d0afc7abdab59c45b7b6f439
Fatih H <fatih.hsu1907@gmail.com>
2022-04-18 00:59:22 +0300

    firebase init

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwCXu6Vu_rUluSOPN5F0OX0_CPx-sGsi8",
+  authDomain: "twitter-clone-3ab94.firebaseapp.com",
+  projectId: "twitter-clone-3ab94",
+  storageBucket: "twitter-clone-3ab94.appspot.com",
+  messagingSenderId: "525962903143",
+  appId: "1:525962903143:web:9fdb3b84954bc861935ec2",
+  measurementId: "G-EPCK3GYR8S"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
