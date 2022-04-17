7482600ddc1e8d411a5f972148a42c5d9db3e3a1
Fatih H <fatih.hsu1907@gmail.com>
2022-04-17 23:54:14 +0300

    Structure is done

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './Widgets.css';
+import SearchIcon from '@mui/icons-material/Search';
+import { TwitterTweetEmbed, TwitterTimelineEmbed, TwitterShareButton } from 'react-twitter-embed';
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's Happening</h2>
+        
+        <TwitterTweetEmbed tweetId="1505611591005446145"/>
+
+        <TwitterTimelineEmbed 
+          sourceType="profile"
+          screenName="elonmusk"
+          options={{height:400}}
+        />
+
+        <TwitterShareButton 
+          url={"https://www.youtube.com/watch?v=rJjaqSTzOxI&t=7979s"}
+          options={{text: "#reactjs is awesome", via:"nextgenturk"}}
+        />
+
+      </div>
     </div>
   )
 }
